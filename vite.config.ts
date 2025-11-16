a956191bc48199406c67c3aaa1af2091147b8762
abinash-swain <swainabinash36@gmail.com>
2025-11-16 23:41:58 +0530

    feat: add Vercel configuration and clean up vite.config.ts
    - Introduced vercel.json for URL rewrites to serve index.html.
    - Cleaned up vite.config.ts by removing commented-out code and improving formatting.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import checker from 'vite-plugin-checker';
+// // https://vite.dev/config/
+// export default defineConfig({
+//    base: '/', 
+//   plugins: [react(), tailwindcss(),
+//     checker({ typescript: true })
+//   ],
+// })
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import checker from 'vite-plugin-checker';
-// https://vite.dev/config/
+import checker from 'vite-plugin-checker'
+
 export default defineConfig({
-   base: '/', 
-  plugins: [react(), tailwindcss(),
+  base: '/',
+  plugins: [
+    react(),
+    tailwindcss(),
     checker({ typescript: true })
   ],
 })
